06319c2a4e0c361680b9e7aee4a31984bd75d568
Alexander <zakrevskyalex1323@gmail.com>
2023-01-12 20:58:03 +0300

    Changed layout to render sections; Reconfigured middleware pipeline to allow authorization and razor pages.

--- bestBuild/Program.cs
+++ bestBuild/Program.cs
@@ -7,6 +7,7 @@ using bestBuild.Areas.Identity.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("SqliteConnection")));
@@ -20,17 +21,27 @@ builder.Services.AddDefaultIdentity<ClientCred>(options =>
 
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
+app.MapRazorPages();
 DbInitializer.Seed(app);
 
 app.Run();
