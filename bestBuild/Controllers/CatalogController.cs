e74f4c5557b33c4339d48756da274de34cfd44d3
Alexander <zakrevskyalex1323@gmail.com>
2023-01-14 23:30:07 +0300

    Added product page

--- bestBuild/Controllers/CatalogController.cs
+++ bestBuild/Controllers/CatalogController.cs
@@ -17,9 +17,11 @@ public class CatalogController : Controller
     [Route("{id:int}")]
     public async Task<IActionResult> Index(int? page, int id)
     {
+        if (context.ProductCategories == null) return Problem("context.ProductCategories is null");
         var ProdCatVm = new ProductCatalogViewModel
         {
             Products = await context.Products.Where(c => c.CategoryId == id).OrderBy(n => n.Name).ToListAsync(),
+            ProductCategory = await context.ProductCategories.Where(i => i.CategoryId == id).FirstOrDefaultAsync()!,
         };
         return View(ProdCatVm);
     }
--- bestBuild/Models/ProductCatalogViewModel.cs
+++ bestBuild/Models/ProductCatalogViewModel.cs
@@ -5,6 +5,7 @@ namespace bestBuild.Models;
 
 public class ProductCatalogViewModel
 {
+    public ProductCategory ProductCategory { get; set; } = null!;
     public List<Product> Products { get; set; } = null!;
     public List<ProductCategory> ProductCategories { get; set; } = null!;
     public string? SearchString { get; set; }
