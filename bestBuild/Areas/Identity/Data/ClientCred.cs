4684d272e7e5f5c74ebe3bec0bc7bd7ab666d052
Alexander <zakrevskyalex1323@gmail.com>
2023-01-16 13:35:44 +0300

    Added user props to ClientCred

--- bestBuild/Areas/Identity/Data/ClientCred.cs
+++ bestBuild/Areas/Identity/Data/ClientCred.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
@@ -9,5 +10,14 @@ namespace bestBuild.Areas.Identity.Data;
 // Add profile data for application users by adding properties to the ClientCred class
 public class ClientCred : IdentityUser
 {
+    [Required]
+    public override string? UserName { get; set; }
+    [Required]
+    public string? UserLastName { get; set; }
+    [Required]
+    public override string? PhoneNumber { get => base.PhoneNumber; set => base.PhoneNumber = value; }
+    [Range(0, 1)]
+    public double PersonalDiscount { get; set; }
+    public double AmoutOfRedemption { get; set; }
 }
 
